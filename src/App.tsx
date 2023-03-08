f58f85aaa70d84dd63561ce57986af23c951fcaa
MTG\mtg09 <mtg.software.dev@gmail.com>
2023-03-08 09:45:43 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="text-h1 text-red-500">HI</h1>
+    </div>
+  );
+}
+
+export default App;
